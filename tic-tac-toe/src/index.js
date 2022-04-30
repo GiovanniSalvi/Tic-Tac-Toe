efc9e06d8e47d721a5fdd9c4bb794972539d4883
GiovanniSalvi <salvigiovanni4@gmail.com>
2022-04-30 04:10:57 +0000

    first commit

---
+++ tic-tac-toe/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './index.css';
+import ReactDom from "react-dom";
+import Game from "./Components/Game";
+
+ReactDom.render(<Game />, document.getElementById("root"));
+
